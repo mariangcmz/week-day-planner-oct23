887b378e439b1991621ac44cd87d2a085c7d52ed
mariangcmz <mariangcmz@gmail.com>
2023-11-06 18:12:02 -0600

    added today's date

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,10 @@
 var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 var container = document.querySelector(".container-fluid")
 var timeblockTemplate = document.querySelector("#timeblock-template")
-
-
-
+var todaysDate = new Date().toLocaleDateString("en-US", { weekday: "short", day: "numeric", month: "long", year: "numeric" })
+console.log(todaysDate)
+var currentDay =document.getElementById("currentDay")
+currentDay.textContent = todaysDate
 
 for (let index = 0; index < hours.length; index++) {
     const element = hours[index];
